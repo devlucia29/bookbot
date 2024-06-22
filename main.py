115ab88b9ad1f7dbceef5e3a400b4b1372e25f10
Ana Lucia Castellanos Vargas <webdev.lucia@gmail.com>
2024-06-21 21:27:58 -0600

    counting characters

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 def main():
-  file_path = "books/frankenstein.txt"
-  content = read_file_content(file_path)
-  total_words = numbers_of_words(content)
-  print(total_words)
+    file_path = "books/frankenstein.txt"
+    content = read_file_content(file_path)
+    total_words = numbers_of_words(content)
+    total_characters = numbers_of_characters(content)
+    print(total_characters)
+
 
 def read_file_content(path):
-  with open(path) as f:
-    file_contents = f.read()
-  return file_contents
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+
 
 def numbers_of_words(text):
-  words = text.split()
-  return len(words)
+    words = text.split()
+    return len(words)
+
+
+def numbers_of_characters(text):
+    dictionary_of_characters = {}
+    for character in text:
+        char = character.lower()
+        if char in dictionary_of_characters:
+            dictionary_of_characters[char] += 1
+        else:
+            dictionary_of_characters[char] = 1
+    return dictionary_of_characters
+
 
 main()
-
-
-
-
-
-    
